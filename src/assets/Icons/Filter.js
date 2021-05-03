c40e3680b108d8b69972bbb1b7bcc995851390ec
Jayanth Karri <jayanthkarri@Jayanths-MacBook-Pro.local>
2021-05-03 09:52:06 +0530

    Added Icons, Utils and Header Component

---
+++ src/assets/Icons/Filter.js
@@ -0,0 +1,22 @@
+import * as React from "react";
+import PxToRem from "../../utils/PxToRem";
+
+function Filter(props) {
+    return (
+        <svg
+            width={PxToRem(12)}
+            height={PxToRem(13)}
+            viewBox="0 0 12 13"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            {...props}
+        >
+            <path
+                d="M12 .667V2h-.667L8 7v5.667H4V7L.667 2H0V.667h12zM2.27 2l3.063 4.596v4.737h1.334V6.596L9.73 2H2.269z"
+                fill="#4B5563"
+            />
+        </svg>
+    )
+}
+
+export default Filter;
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import Header from '../../components/common/Header';
 
-const HomePage = () => (<h1>HOMEPAGE</h1>)
+const HomePage = () => (<Header />)
 
 export default HomePage;

b646fc28a1ae4d380ec28476a34c96d434dbb40f
Jayanth Karri <jayanthkarri@Jayanths-MacBook-Pro.local>
2021-05-15 22:31:59 +0530

    Date Picker component logic completed

--- src/components/common/DatePicker/DatePicker.js
+++ src/components/common/DatePicker/DatePicker.js
@@ -17,16 +17,20 @@ const DatePickerWrapper = styled.div`
 `
 const Select = styled.select`
     font-size: 14px;
+    line-heigt:18px;
+    font-weight:bold;
     -moz-appearance: none;
     -webkit-appearance: none;
     appearance: none;
     border: none;
     outline:none;
+ 
 `
 
 const Seperator = styled.div`
     background: #e4e4e7;
     width:2px;
+    margin-right:${PxToRem(8)};
     height:${PxToRem(257)};
 `
 
@@ -40,13 +44,14 @@ const HorizontalRule = styled.hr`
 const DefaultDatesRanges = ['Past week', 'Past month', 'Past 3 months', 'Past 6 months', 'Past year', 'Past 2 years'];
 
 const DatePicker = ({ isOpen, closeModal }) => {
-    const [date, setDate] = useState(new Date());
-    const handleChange = (date) => [setDate(date)];
-    const years = Array(20).fill().map((_, idx) => date.getFullYear() - idx);
-    const handleEndDateChange = (increaseMonth) => {
-        increaseMonth();
-        closeModal();
-    }
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date(startDate));
+    const handleStartDateChange = (date) => [setStartDate(date), startDate && endDate && startDate != endDate && closeModal()];
+    const handleEndDateChange = (date) => [setEndDate(date), startDate && endDate && startDate != endDate && closeModal()];
+    const years = Array(20).fill().map((_, idx) => endDate.getFullYear() - idx);
+
+    console.log(endDate, startDate)
+
     return (
         <Modal isOpen={isOpen}>
             <DatePickerWrapper style={{ cursor: 'pointer' }}>
@@ -62,13 +67,24 @@ const DatePicker = ({ isOpen, closeModal }) => {
 
                     <ReactDatePicker
                         inline
+                        maxDate={endDate}
                         renderCustomHeader={({ date, decreaseMonth, increaseMonth, changeMonth, changeYear }) => (
                             <>
                                 <Row justifyContent='space-between'>
                                     <LeftArrow onClick={decreaseMonth} style={{ marginLeft: PxToRem(16) }} />
                                     <div style={{ display: 'flex', marginLeft: PxToRem(-48) }}>
-                                        <Text>{monthNames[date.getMonth()]}</Text>
-                                        <SelectIcon onClick={changeMonth} style={{ marginLeft: PxToRem(4) }} />
+                                        <Select
+                                            style={{ padding: 0 }}
+                                            value={monthNames[date.getMonth()]}
+                                            onChange={({ target: { value } }) => changeMonth(monthNames.indexOf(value))}
+                                        >
+                                            {monthNames.map(option => (
+                                                <option key={option} value={option}>
+                                                    {option}
+                                                </option>
+                                            ))}
+                                        </Select>
+                                        <SelectIcon />
                                     </div>
                                     <div style={{ marginRight: PxToRem(24) }}>
                                         <Select
@@ -82,24 +98,35 @@ const DatePicker = ({ isOpen, closeModal }) => {
                                                 </option>
                                             ))}
                                         </Select>
-                                        <SelectIcon onClick={changeMonth} />
+                                        <SelectIcon />
                                     </div>
 
                                 </Row>
                                 <HorizontalRule />
                             </>
                         )}
-                        selected={date}
-                        onChange={handleChange}
+                        selected={new Date(startDate) < new Date(endDate) && startDate}
+                        onChange={handleStartDateChange}
                     />
                     <ReactDatePicker
                         inline
+                        minDate={startDate}
                         renderCustomHeader={({ date, decreaseMonth, increaseMonth, changeMonth, changeYear }) => (
                             <>
                                 <Row justifyContent='space-between'>
                                     <div style={{ display: 'flex', marginLeft: PxToRem(24), width: PxToRem(100) }}>
-                                        <Text>{monthNames[date.getMonth()]}</Text>
-                                        <SelectIcon onClick={changeMonth} style={{ marginLeft: PxToRem(4) }} />
+                                        <Select
+                                            style={{ padding: 0 }}
+                                            value={monthNames[date.getMonth()]}
+                                            onChange={({ target: { value } }) => changeMonth(monthNames.indexOf(value))}
+                                        >
+                                            {monthNames.map(option => (
+                                                <option key={option} value={option}>
+                                                    {option}
+                                                </option>
+                                            ))}
+                                        </Select>
+                                        <SelectIcon />
                                     </div>
                                     <div>
                                         <Select
@@ -113,16 +140,16 @@ const DatePicker = ({ isOpen, closeModal }) => {
                                                 </option>
                                             ))}
                                         </Select>
-                                        <SelectIcon onClick={changeMonth} />
+                                        <SelectIcon />
 
                                     </div>
-                                    <RightArrow onClick={() => handleEndDateChange(increaseMonth)} style={{ marginRight: PxToRem(16) }} />
+                                    <RightArrow onClick={increaseMonth} style={{ marginRight: PxToRem(16) }} />
                                 </Row>
                                 <HorizontalRule />
                             </>
                         )}
-                        selected={date}
-                        onChange={handleChange}
+                        selected={new Date(startDate) < new Date(endDate) && endDate}
+                        onChange={handleEndDateChange}
                     />
                 </Row>
             </DatePickerWrapper>
